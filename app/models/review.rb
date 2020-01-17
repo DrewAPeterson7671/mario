48ef3c5ac7daae0a4af3aac463abf634d64da0ce
Student <example@example.com>
2020-01-17 13:35:56 -0800

    initial validations in reviews for content body fail

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, presence: true
+  validates_length_of :author, maximum: 30
+
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,4 +4,7 @@ describe Review do
   it { should belong_to(:product) }
   it { should validate_presence_of :author }
   it { should validate_length_of(:author).is_at_most(30) }
+  it { should validate_presence_of :content_body }
+  it { should validate_length_of(:content_body).is_at_least(50) }
+  it { should validate_length_of(:content_body).is_at_most(250) }
 end
