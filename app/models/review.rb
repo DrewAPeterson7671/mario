79373d4e2eb46fdc9c7f65030fbd0c53e49cc5f4
Student <example@example.com>
2020-01-17 13:38:14 -0800

    initial validations in reviews for content body pass

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,7 @@ class Review < ApplicationRecord
   belongs_to :product
   validates :author, presence: true
   validates_length_of :author, maximum: 30
-
+  validates :content_body, presence: true
+  validates_length_of :content_body, minimum: 50
+  validates_length_of :content_body, maximum: 250
 end
