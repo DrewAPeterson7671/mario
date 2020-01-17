a2e9696f704e4904fc26ad58ffcac734b156a672
Student <example@example.com>
2020-01-17 13:22:47 -0800

    initial validationss for price pass

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates_length_of :name, maximum: 30
+  validates :price, presence: true
+  validates_length_of :name, maximum: 8
+  
 end
