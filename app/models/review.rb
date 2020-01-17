b567a92fd123aa223c86a41196fb868d96941005
Student <example@example.com>
2020-01-17 13:54:48 -0800

    initial validations in reviews for rating pass

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,9 @@ class Review < ApplicationRecord
   validates :content_body, presence: true
   validates_length_of :content_body, minimum: 50
   validates_length_of :content_body, maximum: 250
+  validates :rating, presence: true
+  validates :rating, presence: true
+  validates_numericality_of :rating, only_integer: true
+  validates_numericality_of :rating, less_than_or_equal_to: 5
+  validates_numericality_of :rating, greater_than_or_equal_to: 1
 end
