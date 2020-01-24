8eaa6085f00dfc31e0a705294cd209fde68de787
Student <example@example.com>
2020-01-24 15:02:32 -0800

    Continue to try to test authentication and also how to test CRUD functionality that has been locked out by admin privleges

--- spec/features/add_product_spec.rb
+++ spec/features/add_product_spec.rb
@@ -1,12 +1,36 @@
 require 'rails_helper'
 
-describe "the add a product process" do
+describe "Unregistered user can view product page" do
   it "adds a new product" do
     visit products_path
     click_link 'Return to products'
-
-    expect(page).to have_content 'Wario'
+    expect(page).to have_content 'Mario'
   end
+end
+
+
+describe "the user registration process" do
+  it "takes user to registration page" do
+    visit products_path
+    click_link 'Sign up'
+    expect(page).to have_content "Email"
+  end
+end
+
+describe "the user registration process" do
+  it "registers a user" do
+    visit products_path
+    click_link 'Sign up'
+    fill_in 'Email', :with => 'chet@chet.com'
+    fill_in 'Password', :with => 'pass'
+    fill_in 'Password confirmation', :with => 'pass'
+    click_on 'Sign Up'
+    expect(page).to have_content "You've successfully signed up!"
+  end
+end
+
+
+
 
 # describe "the add a product process" do
 #   it "adds a new product" do
@@ -19,13 +43,13 @@ describe "the add a product process" do
 #     expect(page).to have_content 'Product successfully added!'
 #     expect(page).to have_content 'Grapes'
 #   end
-#
+# end
+
+
 #   it "gives an error when no name is entered" do
 #     visit new_product_path
 #     click_on 'Create Product'
 #     expect(page).to have_content "Name can't be blank"
 #     expect(page).to have_content "Price can't be blank"
 #     expect(page).to have_content "Country can't be blank"
-#   end
-
-end
+  # end
