c7f0ea9118380a536393c31676e83504047cb879
Student <example@example.com>
2020-01-24 14:20:33 -0800

    Have meaningful fail to putting unregistered user back to products

--- spec/features/add_product_spec.rb
+++ spec/features/add_product_spec.rb
@@ -3,21 +3,29 @@ require 'rails_helper'
 describe "the add a product process" do
   it "adds a new product" do
     visit products_path
-    click_link 'Create new product'
-    fill_in 'Name', :with => 'Grapes'
-    fill_in 'Price', :with => '3.46'
-    fill_in 'Country', :with => 'Brazil'
-    click_on 'Create Product'
-    expect(page).to have_content 'Product successfully added!'
-    expect(page).to have_content 'Grapes'
+    click_link 'Return to products'
+
+    expect(page).to have_content 'Wario'
   end
 
-  it "gives an error when no name is entered" do
-    visit new_product_path
-    click_on 'Create Product'
-    expect(page).to have_content "Name can't be blank"
-    expect(page).to have_content "Price can't be blank"
-    expect(page).to have_content "Country can't be blank"
-  end
+# describe "the add a product process" do
+#   it "adds a new product" do
+#     visit products_path
+#     click_link 'Create new product'
+#     fill_in 'Name', :with => 'Grapes'
+#     fill_in 'Price', :with => '3.46'
+#     fill_in 'Country', :with => 'Brazil'
+#     click_on 'Create Product'
+#     expect(page).to have_content 'Product successfully added!'
+#     expect(page).to have_content 'Grapes'
+#   end
+#
+#   it "gives an error when no name is entered" do
+#     visit new_product_path
+#     click_on 'Create Product'
+#     expect(page).to have_content "Name can't be blank"
+#     expect(page).to have_content "Price can't be blank"
+#     expect(page).to have_content "Country can't be blank"
+#   end
 
 end
