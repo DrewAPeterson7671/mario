8364772346862f9bee6f9e825e5a56a433e9b757
Student <example@example.com>
2020-01-24 09:12:36 -0800

    Edit scaffold's  user controllers, files, views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+  
   resources :users
   root to: 'products#index'
   resources :products do
--- db/migrate/20200124161758_create_users.rb
+++ db/migrate/20200124161758_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.column :email, :string
+      t.column :password_hash, :string
+      t.column :password_salt, :string
+      t.column :user_name, :string
+      t.column :admin, :boolean         
       t.timestamps
     end
   end
